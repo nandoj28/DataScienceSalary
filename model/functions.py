b1201f677e6fbc6064fa4bb166756a7f8675440a
nandoj28 <116756269+nandoj28@users.noreply.github.com>
2024-02-19 21:54:19 -0600

    more FE2

---
+++ model/functions.py
@@ -0,0 +1,22 @@
+region_mapping = {
+    'US': 'North America', 'CA': 'North America',
+    'AR': 'Latin', 'BO': 'Latin', 'BR': 'Latin', 'CL': 'Latin', 'CO': 'Latin', 'CR': 'Latin', 'CU': 'Latin', 
+    'DO': 'Latin', 'EC': 'Latin', 'GT': 'Latin', 'HN': 'Latin', 'MX': 'Latin', 'NI': 'Latin', 'PA': 'Latin', 
+    'PE': 'Latin', 'PR': 'Latin', 'PY': 'Latin', 'SV': 'Latin', 'UY': 'Latin', 'VE': 'Latin', 'BS': 'Latin',
+    'AT': 'Western Europe', 'BE': 'Western Europe', 'CH': 'Western Europe', 'DE': 'Western Europe', 'DK': 'Western Europe', 
+    'ES': 'Western Europe', 'FI': 'Western Europe', 'FR': 'Western Europe', 'GB': 'Western Europe', 'IE': 'Western Europe', 
+    'IS': 'Western Europe', 'IT': 'Western Europe', 'LU': 'Western Europe', 'NL': 'Western Europe', 'NO': 'Western Europe', 
+    'PT': 'Western Europe', 'SE': 'Western Europe', 'AD': 'Western Europe', 'JE': 'Western Europe', 'MT': 'Western Europe', 'GI': "Western Europe",
+    'BG': 'Eastern Europe', 'CZ': 'Eastern Europe', 'EE': 'Eastern Europe', 'HU': 'Eastern Europe', 'LV': 'Eastern Europe', 
+    'LT': 'Eastern Europe', 'PL': 'Eastern Europe', 'RO': 'Eastern Europe', 'RU': 'Eastern Europe', 'SI': 'Eastern Europe', 
+    'RS': 'Eastern Europe', 'BA': 'Eastern Europe', 
+    'SK': 'Eastern Europe', 'UA': 'Eastern Europe', 'HR': 'Eastern Europe', 'GR': 'Eastern Europe',  'MD': 'Eastern Europe',
+    'AE': 'Middle East', 'AM': 'Middle East', 'AZ': 'Middle East', 'BH': 'Middle East', 'GE': 'Middle East', 'IL': 'Middle East', 
+    'IQ': 'Middle East', 'IR': 'Middle East', 'JO': 'Middle East', 'KW': 'Middle East', 'LB': 'Middle East', 'OM': 'Middle East', 
+    'QA': 'Middle East', 'SA': 'Middle East', 'SY': 'Middle East', 'TR': 'Middle East', 'YE': 'Middle East', 'CY': 'Middle East', 
+    'DZ': 'Africa', 'EG': 'Africa', 'KE': 'Africa', 'MA': 'Africa', 'NG': 'Africa', 'ZA': 'Africa', 'TN': 'Africa', 'UG': 'Africa', 
+    'GH': 'Africa', 'MU': 'Africa', 'CF': 'Africa',
+    'CN': 'Asia', 'IN': 'Asia', 'ID': 'Asia', 'JP': 'Asia', 'KR': 'Asia', 'MY': 'Asia', 'PH': 'Asia', 'SG': 'Asia', 'TH': 'Asia', 
+    'VN': 'Asia', 'HK': 'Asia', 'TW': 'Asia', 'PK': 'Asia', 'UZ': 'Asia',
+    'AU': 'Oceania', 'NZ': 'Oceania', 'AS': 'Oceania'
+}
